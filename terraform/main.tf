db00bbb9a047f263a6ccb58d9a7efdc9b32f0080
Sohaib Ismail <sohaib.ismail@atomic-computing.com>
2023-08-14 12:12:59 +0500

    Creating first aft account using terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "landingzone+Sandbox-acc@iconnect.technology"
     AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "landingzone+Sandbox-acc@iconnect.technology"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
